c1eed16e672bad4af3c82f3e47821f1e26b8f16c
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-25 19:34:09 +0600

    fix secret key nt getting issue

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,12 +1,13 @@
-const router = require('express').Router();
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-router.post('/jwt', async (req, res) => {
+router.post("/jwt", async (req, res) => {
 	try {
 		const user = req.body;
+		console.log(user);
 		const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-			expiresIn: '1h',
+			expiresIn: "1h",
 		});
 		res.send({ token });
 	} catch (error) {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 // Import models
-const User = require('../models/users');
+const User = require("../models/users");
 
 // Get all users
-router.get('/users', async (req, res) => {
+router.get("/users", async (req, res) => {
 	const users = await User.find();
 	res.send(users);
 });
 
 // get specific user by email address
-router.get('/users/:email', async (req, res) => {
+router.get("/users/:email", async (req, res) => {
 	const user = await User.findOne({ email: req.params.email });
 	res.send(user.role);
 });
 
 // Get specific user by email and update role use mongoose findByIdAndUpdate method
 
-router.patch('/users/:id', async (req, res) => {
+router.patch("/users/:id", async (req, res) => {
 	const userId = req.params.id;
 	const newRole = req.body.role;
 	try {
@@ -29,30 +29,30 @@ router.patch('/users/:id', async (req, res) => {
 		);
 		res.send(updatedUser);
 		if (!updatedUser) {
-			return res.status(404).json({ message: 'User not found' });
+			return res.status(404).json({ message: "User not found" });
 		}
 	} catch (error) {
-		console.error('Error updating role:', error);
-		res.status(500).json({ message: 'Internal Server Error' });
+		console.error("Error updating role:", error);
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
 // Post a new user
 
-router.post('/users', async (req, res) => {
+router.post("/users", async (req, res) => {
 	try {
 		const userData = req.body;
 		const existingUser = await User.findOne({ email: userData.email });
 		if (existingUser) {
-			console.log('User already exists');
-			return res.send('User with this email already exists');
+			console.log("User already exists");
+			return res.send("User with this email already exists");
 		}
 		const newUser = new User(req.body);
 		const result = await newUser.save();
 		res.send(result);
 	} catch (error) {
-		console.error('Error saving user:', error);
-		res.status(500).send('Internal Server Error');
+		console.error("Error saving user:", error);
+		res.status(500).send("Internal Server Error");
 	}
 });
 
